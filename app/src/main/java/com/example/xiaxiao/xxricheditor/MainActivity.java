29dd43c640b38ff01f8bf0b1c81a07c25d33840b
xiaxiao <xiaxiao@lis99.com>
2018-03-15 15:16:44 +0800

    解决选项的UI样式和文本的对应变化，纯文本阶段已完成

--- app/src/main/java/com/example/xiaxiao/xxricheditor/MainActivity.java
+++ app/src/main/java/com/example/xiaxiao/xxricheditor/MainActivity.java
@@ -11,24 +11,24 @@ import com.example.xiaxiao.xxricheditor.textstyle.SizeStyle;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditorLayout editorLayout;
-    TextView boldView   ;
+    TextView boldView;
     TextView italicView;
     TextView deleteView;
     TextView quoteView;
-    TextView sizeView1 ;
-    TextView sizeView2 ;
-    TextView sizeView3 ;
-    TextView sizeView4 ;
+    TextView sizeView1;
+    TextView sizeView2;
+    TextView sizeView3;
+    TextView sizeView4;
     List<TextView> sizeViews = new ArrayList();
-    boolean bold=false;
-    boolean italic=false;
-    boolean deleteLine=false;
+    boolean bold = false;
+    boolean italic = false;
+    boolean deleteLine = false;
     int textSize = SizeStyle.TEXT_SIZE_NORMAL;
     RichEditorManager richEditorManager;
-    private boolean quote=false;
+    private boolean quote = false;
     private StyleController.StyleParams styleParams;
 
     @Override
@@ -38,20 +38,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         editorLayout = (EditorLayout) findViewById(R.id.editor_ll);
         richEditorManager = new RichEditorManager(editorLayout);
 
-        boldView    =(TextView)findViewById(R.id.boldView);
-        italicView  =(TextView)findViewById(R.id.italicView);
-        deleteView  =(TextView)findViewById(R.id.delete_line);
-        quoteView   =(TextView)findViewById(R.id.quote_tv);
-        sizeView1   =(TextView)findViewById(R.id.sizeView1);
-        sizeView2   =(TextView)findViewById(R.id.sizeView2);
-        sizeView3   =(TextView)findViewById(R.id.sizeView3);
-        sizeView4   =(TextView)findViewById(R.id.sizeView4);
+        boldView = (TextView) findViewById(R.id.boldView);
+        italicView = (TextView) findViewById(R.id.italicView);
+        deleteView = (TextView) findViewById(R.id.delete_line);
+        quoteView = (TextView) findViewById(R.id.quote_tv);
+        sizeView1 = (TextView) findViewById(R.id.sizeView1);
+        sizeView2 = (TextView) findViewById(R.id.sizeView2);
+        sizeView3 = (TextView) findViewById(R.id.sizeView3);
+        sizeView4 = (TextView) findViewById(R.id.sizeView4);
         sizeViews.add(sizeView1);
         sizeViews.add(sizeView2);
         sizeViews.add(sizeView3);
         sizeViews.add(sizeView4);
-
-
 
 
         findViewById(R.id.boldView).setOnClickListener(this);
@@ -76,93 +74,96 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
         editorLayout.ready();
-		//实时跟新选项item的UI样式还有问题
+        //实时跟新选项item的UI样式还有问题
     }
 
     private void changeBottonItems(StyleController.StyleParams styleParams) {
-        selectView(boldView,styleParams.boldStyle.getStatus());
-        selectView(italicView,styleParams.italicStyle.getStatus());
-        selectView(deleteView,styleParams.deleteLineStyle.getStatus());
-        selectView(quoteView,styleParams.quoteStyle.getStatus());
+        selectView(boldView, styleParams.boldStyle.getStatus());
+        selectView(italicView, styleParams.italicStyle.getStatus());
+        selectView(deleteView, styleParams.deleteLineStyle.getStatus());
+        selectView(quoteView, styleParams.quoteStyle.getStatus());
         int size = styleParams.sizeStyle.getSize();
-        selectSizeView(sizeView1,size==SizeStyle.TEXT_SIZE_SUPER_LARGE);
-        selectSizeView(sizeView2,size==SizeStyle.TEXT_SIZE_LARGE);
-        selectSizeView(sizeView3,size==SizeStyle.TEXT_SIZE_SMALL);
-        selectSizeView(sizeView4,size==SizeStyle.TEXT_SIZE_SUPER_SMALL);
+        selectSizeView(size);
+
+        bold = styleParams.boldStyle.getStatus();
+        italic = styleParams.italicStyle.getStatus();
+        deleteLine = styleParams.deleteLineStyle.getStatus();
+        quote = styleParams.quoteStyle.getStatus();
+        textSize = styleParams.sizeStyle.getSize();
     }
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        if(id==R.id.boldView){
-            bold = styleParams.boldStyle.getStatus();
-//            bold = !bold;
-            selectView((TextView)v,bold);
+        if (id == R.id.boldView) {
+//            bold = styleParams.boldStyle.getStatus();
+            bold = !bold;
+            selectView((TextView) v, bold);
 
             richEditorManager.bold().commit();
-            selectView((TextView)v,bold);
+            selectView((TextView) v, bold);
         }
-        if(id==R.id.italicView){
-            italic = styleParams.italicStyle.getStatus();
-//            italic = !italic;
-            selectView((TextView)v,italic);
+        if (id == R.id.italicView) {
+//            italic = styleParams.italicStyle.getStatus();
+            italic = !italic;
+            selectView((TextView) v, italic);
             richEditorManager.italic().commit();
         }
 
         textSize = styleParams.sizeStyle.getSize();
-        if(id==R.id.sizeView1){
+        if (id == R.id.sizeView1) {
             if (textSize == SizeStyle.TEXT_SIZE_SUPER_LARGE) {
                 textSize = SizeStyle.TEXT_SIZE_NORMAL;
-                selectSizeView((TextView)v,false);
+                selectSizeView(textSize);
             } else {
                 textSize = SizeStyle.TEXT_SIZE_SUPER_LARGE;
-                selectSizeView((TextView)v,true);
+                selectSizeView(textSize);
             }
             richEditorManager.textSize(textSize).commit();
         }
-        if(id==R.id.sizeView2){
+        if (id == R.id.sizeView2) {
             if (textSize == SizeStyle.TEXT_SIZE_LARGE) {
                 textSize = SizeStyle.TEXT_SIZE_NORMAL;
-                selectSizeView((TextView)v,false);
+                selectSizeView(textSize);
             } else {
                 textSize = SizeStyle.TEXT_SIZE_LARGE;
-                selectSizeView((TextView)v,true);
+                selectSizeView(textSize);
             }
             richEditorManager.textSize(textSize).commit();
         }
-        if(id==R.id.sizeView3){
+        if (id == R.id.sizeView3) {
             if (textSize == SizeStyle.TEXT_SIZE_SMALL) {
                 textSize = SizeStyle.TEXT_SIZE_NORMAL;
-                selectSizeView((TextView)v,false);
+                selectSizeView(textSize);
             } else {
                 textSize = SizeStyle.TEXT_SIZE_SMALL;
-                selectSizeView((TextView)v,true);
+                selectSizeView(textSize);
             }
             richEditorManager.textSize(textSize).commit();
         }
-        if(id==R.id.sizeView4){
+        if (id == R.id.sizeView4) {
             if (textSize == SizeStyle.TEXT_SIZE_SUPER_SMALL) {
                 textSize = SizeStyle.TEXT_SIZE_NORMAL;
-                selectSizeView((TextView)v,false);
+                selectSizeView(textSize);
             } else {
                 textSize = SizeStyle.TEXT_SIZE_SUPER_SMALL;
-                selectSizeView((TextView)v,true);
+                selectSizeView(textSize);
             }
             richEditorManager.textSize(textSize).commit();
         }
-        if (id==R.id.delete_line) {
-            deleteLine = styleParams.deleteLineStyle.getStatus();
-//            deleteLine = !deleteLine;
-            selectView((TextView)v,deleteLine);
+        if (id == R.id.delete_line) {
+//            deleteLine = styleParams.deleteLineStyle.getStatus();
+            deleteLine = !deleteLine;
+            selectView((TextView) v, deleteLine);
 
-           richEditorManager.deleteLine().commit();
+            richEditorManager.deleteLine().commit();
         }
-        if (id==R.id.quote_tv) {
-            quote = styleParams.quoteStyle.getStatus();
-//            quote = !quote;
-            selectView((TextView)v,quote);
+        if (id == R.id.quote_tv) {
+//            quote = styleParams.quoteStyle.getStatus();
+            quote = !quote;
+            selectView((TextView) v, quote);
 
-           richEditorManager.quote().commit();
+            richEditorManager.quote().commit();
         }
 
     }
@@ -179,16 +180,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             textView.setBackgroundResource(0);
         }
     }
-    public void selectSizeView(TextView textView, boolean select) {
-        for (TextView sizeView : sizeViews) {
-            textView.setTextColor(Color.BLUE);
-            textView.setBackgroundResource(0);
 
-            if (select&&sizeView==textView) {
-                sizeView.setTextColor(Color.RED);
-                textView.setBackgroundResource(R.drawable.item_bg);
+    public void selectSizeView(int size) {
+        /*for (TextView t : sizeViews) {
+            t.setTextColor(Color.BLUE);
+            t.setBackgroundResource(0);
+        }
+        if (select) {
+            for (TextView tt : sizeViews) {
+                if (tt == textView) {
+                    tt.setTextColor(Color.RED);
+                    tt.setBackgroundResource(R.drawable.item_bg);
+                }
             }
+        }*/
+        if (size == SizeStyle.TEXT_SIZE_SUPER_LARGE) {
+            sizeViews.get(0).setTextColor(Color.RED);
+            sizeViews.get(0).setBackgroundResource(R.drawable.item_bg);
+        } else {
+            sizeViews.get(0).setTextColor(Color.BLUE);
+            sizeViews.get(0).setBackgroundResource(0);
+        }
 
+        if (size == SizeStyle.TEXT_SIZE_LARGE) {
+            sizeViews.get(1).setTextColor(Color.RED);
+            sizeViews.get(1).setBackgroundResource(R.drawable.item_bg);
+        } else {
+            sizeViews.get(1).setTextColor(Color.BLUE);
+            sizeViews.get(1).setBackgroundResource(0);
+        }
+
+        if (size == SizeStyle.TEXT_SIZE_SMALL) {
+            sizeViews.get(2).setTextColor(Color.RED);
+            sizeViews.get(2).setBackgroundResource(R.drawable.item_bg);
+        } else {
+            sizeViews.get(2).setTextColor(Color.BLUE);
+            sizeViews.get(2).setBackgroundResource(0);
+        }
+
+        if (size == SizeStyle.TEXT_SIZE_SUPER_SMALL) {
+            sizeViews.get(3).setTextColor(Color.RED);
+            sizeViews.get(3).setBackgroundResource(R.drawable.item_bg);
+        } else {
+            sizeViews.get(3).setTextColor(Color.BLUE);
+            sizeViews.get(3).setBackgroundResource(0);
         }
     }
 }
